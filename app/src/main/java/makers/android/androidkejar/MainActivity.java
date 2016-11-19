a3bf4e512d19d43e37fac9015ebddfda1fbc3b03
FirmanMFK <firman.mfk@gmail.com>
2016-11-19 17:04:20 +0700

    Commit Just Java

--- app/src/main/java/makers/android/androidkejar/MainActivity.java
+++ app/src/main/java/makers/android/androidkejar/MainActivity.java
@@ -1,13 +1,35 @@
 package makers.android.androidkejar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private int jumlah_pesan = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void kurang(View view) {
+        TextView jumlah =(TextView) findViewById(R.id.quantity_text_view);
+        jumlah_pesan = jumlah_pesan - 1;
+        jumlah.setText(String.valueOf(jumlah_pesan));
+    }
+
+    public void tambah(View view) {
+        TextView jumlah = (TextView) findViewById(R.id.quantity_text_view);
+        jumlah_pesan = jumlah_pesan + 1;
+        jumlah.setText(String.valueOf(jumlah_pesan));
+    }
+
+    public void hitung(View view) {
+        int bayar = jumlah_pesan * 2;
+        TextView bayarTextView = (TextView) findViewById(R.id.price_text_view);
+        bayarTextView.setText(String.valueOf(bayar));
     }
 }
